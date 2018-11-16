2dd27863d0e9f7c84ec8a7fa02112a7eb94226b0
prem makvana <prem.makvana@codezeros.com>
2018-11-16 19:24:39 +0530

    updated code

---
+++ server.js
@@ -0,0 +1,28 @@
+var mongoose=require('mongoose')
+var express=require('express')
+var config_index = require('./lib/config/index')
+var app = express()
+var bodyparser = require('body-parser')
+app.use(bodyparser.json())
+require('./lib/routes')(app)
+
+mongoose.connect(config_index.URL,{userNewUrlParser:true})
+app.listen(config_index.port,()=>
+{
+	console.log(`server started `)
+})
+
+ 
+
+// var mongoose=require('mongoose')
+// var express=require('express')
+// var config_index = require('./lib/config')
+// var app = express()
+// var bodyparser = require('body-parser')
+// app.use(bodyparser.json())
+// var listen = require('./lib/config/expressConfig')
+// require('./lib/routes')(app)
+// app.listen;
+// var connect=require('./lib/config/mongoconfig')
+// mongoose.connect;
+
