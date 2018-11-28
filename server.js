d95f411fd14bec3a5557ae5229f66782fba50aae
prem makvana <prem.makvana@codezeros.com>
2018-11-28 13:25:10 +0530

    backup

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express=require('express')
 var config_index = require('./lib/config/index')
 var app = express()
 var bodyparser = require('body-parser')
-app.use(bodyparser.json())
+var env = require('dotenv').config()
+app.use(bodyparser.json({extended:true,limit:'50mb'}))
+app.use(bodyparser.urlencoded({extended:true,limit:'50mb'}))
 require('./lib/routes')(app)
 
 mongoose.connect(config_index.URL,{userNewUrlParser:true})
@@ -13,6 +15,8 @@ app.listen(config_index.port,()=>
 })
 
  
+
+
 
 // var mongoose=require('mongoose')
 // var express=require('express')
